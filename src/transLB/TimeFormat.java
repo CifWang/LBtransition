22327f90f8408b37fb4dcde8918a26c062afb7c8
CifWang <wcf@nssc.ac.cn>
2018-09-17 15:13:45 +0800

    Transit geodetic(latitude, longitude ,altitude) coordinates to LB coordinates

---
+++ src/transLB/TimeFormat.java
@@ -0,0 +1,30 @@
+package transLB;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * 处理时间
+ * @author xcc
+ *
+ */
+public class TimeFormat {
+	
+	public final String timeZone="Etc/GMT+0";
+	private String timestr;
+	
+	public TimeFormat(double t){
+		Date date=new Date((long)t);
+		SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		sf.setTimeZone(TimeZone.getTimeZone(timeZone));
+		String time=sf.format(date);
+		this.timestr=time;
+	}
+	
+	public String getTimestr(){
+		return this.timestr;
+	}
+	
+}
